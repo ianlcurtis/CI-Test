dd351ba37ab3d682a7bb06a77afbd1a12b3ed652
ianlcurtis <i.curtis@elsevier.com>
2016-01-19 15:04:17 +0000

    First commit

---
+++ CI-Test-Library/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_Test_Library
+{
+    public class Class1
+    {
+        public static string GetText()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ CI-Test/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CI_Test_Library;
+
+namespace CI_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Class1.GetText());
+        }
+    }
+}
