b17a8037e1e766a2d3cfc1e2ca57ccae9aa413f2
ianlcurtis <i.curtis@elsevier.com>
2016-01-20 11:13:34 +0000

    Added NUnit test project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using CI_Test_Library;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test_GetText()
+        {
+            Assert.AreEqual("Hello World!", Class1.GetText());
+        }
+    }
+}
