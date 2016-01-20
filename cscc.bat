02176e79d2eae19dd1adf97d11feed95ff0260e4
ianlcurtis <i.curtis@elsevier.com>
2016-01-20 10:17:20 +0000

    Updated build script

--- cscc.bat
+++ cscc.bat
@@ -6,4 +6,4 @@ REM Compile CI-Test-Library
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:library /out:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test-Library\*.cs"
 
 REM Compile CI-Test
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:exe /out:"%WORKSPACE%\CI-Test\bin\Release\CI-Test.exe" /lib:"%WORKSPACE%\CI-Test-Library\bin\Release\" /reference:"CI-Test-Library.dll" "%WORKSPACE%\CI-Test\*.cs"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:exe /out:"%WORKSPACE%\CI-Test\bin\Release\CI-Test.exe" /reference:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test\*.cs"
