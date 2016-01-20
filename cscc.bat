557a40a3549a8f9a90fa49e8da90eb47e64888f4
ianlcurtis <i.curtis@elsevier.com>
2016-01-20 11:22:36 +0000

    Added bins

--- cscc.bat
+++ cscc.bat
@@ -7,3 +7,6 @@ REM Compile CI-Test-Library
 
 REM Compile CI-Test
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:exe /out:"%WORKSPACE%\CI-Test\bin\Release\CI-Test.exe" /reference:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test\*.cs"
+
+REM Compile UnitTestProject1
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:library /out:"%WORKSPACE%\UnitTestProject1\bin\Release\UnitTestProject1.dll" /reference:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\UnitTestProject1\*.cs"
