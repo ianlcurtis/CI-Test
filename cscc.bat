e261bf0012a3e53fa3383edbcf39da29ee7e6c34
ianlcurtis <i.curtis@elsevier.com>
2016-01-21 13:25:46 +0000

    xcopy of nunit.dll

--- cscc.bat
+++ cscc.bat
@@ -12,3 +12,4 @@ REM Compile CI-Test
 
 REM Compile UnitTestProject1
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:library /out:"%WORKSPACE%\UnitTestProject1\bin\Release\UnitTestProject1.dll" /reference:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" /reference:"%WORKSPACE%\References\nunit.framework.dll" "%WORKSPACE%\UnitTestProject1\*.cs"
+xcopy /y "%WORKSPACE%\References\nunit.framework.dll" "%WORKSPACE%\UnitTestProject1\bin\Release\*"
