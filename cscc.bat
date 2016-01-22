2eea4cf63850cde6e611e15fa0f4534519d5ad4e
ianlcurtis <i.curtis@elsevier.com>
2016-01-22 15:26:34 +0000

    x

--- cscc.bat
+++ cscc.bat
@@ -3,7 +3,8 @@ REM Set environment variables
 REM "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
 
 REM Compile CI-Test-Library
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:library /out:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test-Library\*.cs"
+REM "C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc" /target:library /out:"%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test-Library\*.cs"
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%WORKSPACE%\CI-Test-Library\CI-Test-Library.csproj"
 xcopy /y "%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\CI-Test\bin\Release\*"
 xcopy /y "%WORKSPACE%\CI-Test-Library\bin\Release\CI-Test-Library.dll" "%WORKSPACE%\UnitTestProject1\bin\Release\*"
 
